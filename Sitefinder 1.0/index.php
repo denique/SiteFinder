64a35b5ec4af4eca3d8bc0872c3be9944e8375b9
INFO3490-GroupE <38600940+INFO3490-GroupE@users.noreply.github.com>
2018-04-22 07:40:54 -0300

    Correct file

--- Sitefinder 1.0/index.php
+++ Sitefinder 1.0/index.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "user1";
-$password = "";
+$password = "resu";
 $dbname = "sitefinderRooms";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password);
